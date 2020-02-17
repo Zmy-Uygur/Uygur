0cd198b3122b00eeceeb677969e8ff4c8dd22ed4
Zmy-Uygur <60862150+Zmy-Uygur@users.noreply.github.com>
2020-02-17 18:05:18 +0800

    Update dicts.py

--- generate_uygur_test/TextRecognitionDataGenerator/dicts.py
+++ generate_uygur_test/TextRecognitionDataGenerator/dicts.py
@@ -16,27 +16,55 @@
 
 
 
-import random
-from PIL import Image, ImageColor, ImageFont, ImageDraw, ImageFilter
+# import random
+# from PIL import Image, ImageColor, ImageFont, ImageDraw, ImageFilter
+#
+#
+# def randomcolor():
+#     colorArr = ['1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F']
+#     color = ""
+#     for i in range(6):
+#         color += colorArr[random.randint(0, 14)]
+#     return "#" + color
+#
+#
+# # text_color = '#282828'
+# text_color = randomcolor()
+# colors = []
+# for c in text_color.split(','):
+#     colors.append(ImageColor.getrgb(c))
+# # colors = [ImageColor.getrgb(c) for c in text_color.split(',')]
+# c1, c2 = colors[0], colors[-1]
+#
+# fill = (random.randint(c1[0], c2[0]),
+#         random.randint(c1[1], c2[1]),
+#         random.randint(c1[2], c2[2]))
+# print(fill)
 
 
-def randomcolor():
-    colorArr = ['1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F']
-    color = ""
-    for i in range(6):
-        color += colorArr[random.randint(0, 14)]
-    return "#" + color
-
-
-# text_color = '#282828'
-text_color = randomcolor()
-colors = []
-for c in text_color.split(','):
-    colors.append(ImageColor.getrgb(c))
-# colors = [ImageColor.getrgb(c) for c in text_color.split(',')]
-c1, c2 = colors[0], colors[-1]
-
-fill = (random.randint(c1[0], c2[0]),
-        random.randint(c1[1], c2[1]),
-        random.randint(c1[2], c2[2]))
-print(fill)
+# import cv2
+# import numpy as np
+# from PIL import Image
+#
+# img_path = './bg_images/014.jpeg'
+# image = Image.open(img_path)
+#
+# # 要提取的主要颜色数量
+# num_colors = 1
+#
+# small_image = image.resize((image.size))
+# result = small_image.convert('P', palette=Image.ADAPTIVE, colors=num_colors)  # image with 5 dominating colors
+# result = result.convert('RGB')
+# main_colors = result.getcolors(image.width*image.height)
+# print(main_colors)
+#
+# # 提取的主要颜色
+# for count, col in main_colors:
+#     print(count, col)
+#     if count < 40:
+#         continue
+#     a = np.zeros((224, 224, 3))
+#     a = a + np.array(col)
+#     #print(a.astype(np.uint8)[:, :, ::-1][1,1,:])
+#     cv2.imshow('a', a.astype(np.uint8)[:, :, ::-1])
+#     cv2.waitKey()
